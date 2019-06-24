65ba680d05d2b5627b6882d5f060f236cbee7db6
d9394 <9394952@gmail.com>
2019-06-24 09:45:23 +0800

    Update epubee_book.py

--- epubee_book.py
+++ epubee_book.py
@@ -10,6 +10,7 @@ import json
 import random
 import time
 #from spider import myipAgent
+from send_mail import send_mail
 
 headers = {
 	'Host': 'cn.epubee.com',
@@ -55,12 +56,13 @@ def getCookie(proxy):
 	url = u'http://cn.epubee.com//keys/genid_with_localid.asmx/genid_with_localid'
 	data = {'localid': ''}
 	try:
-		response = requests.post(url, json=data, cookies=cookie, proxies=proxy)
+		response = requests.post(url, json=data, cookies=cookie, proxies=proxy, timeout=10)
 		if response.status_code == 200:
 			update_cookies(response.content.decode())
 	except Exception as e:
 		print(u"获取cookie失败！")
-	return response.status_code
+	else :
+		return response.status_code
 
 def cookie_toString(cookie):
 	cookie_str=''
@@ -75,7 +77,7 @@ def add_Book(cookie, bookid, proxy):
 	act = 'search'
 	url = u'http://cn.epubee.com/app_books/addbook.asmx/online_addbook'
 	data = {'bookid': bookid, 'uid': uid, 'act': act}
-	my_header = headers
+	my_header = header
 	my_header['Cookie'] = cookie_str
 	my_header['Connection'] = 'keep-alive'
 	my_header['Content-Type'] = 'application/json'
@@ -165,47 +167,61 @@ def refreshpage(cookie, proxy):
 
 def download(filename, bid, cookie,loc,proxy, size):
 	retry_times = 1
+	return_code = ''
 	filename = loc + filename
 	cookie_str = cookie_toString(cookie)
 	uid = str(cookie.get('identify'))
 	t_key = gett_key(bid,proxy)
 	url = u'http://cn.epubee.com/getFile.ashx?bid=' + bid + u'&uid=' + uid + u'&t_key=' + t_key
-	my_header = header
+	my_header = headers
 	my_header['Cookie'] = cookie_str
 	my_header['Referer' ] = 'http://cn.epubee.com/files.aspx'
 	while retry_times > 0:
 		retry_times -= 1
-		try:
+#		try:
+		if 1 :
 			response = requests.get(url, headers=my_header,proxies=proxy)
+#		except Exception as e:
+#			print(u'下载错误 (%s)' % e)
+#		else:
 			if response.status_code==200:
 				down = response.content
-				if down.find("Ctrl+F5") :
-					print(u'%s' % down)
-					refreshpage(cookie, proxy)
-					time.sleep(3)
-					continue
+				with open(filename, 'wb') as code:
+					code.write(down)
+#				if down.find("Ctrl+F5") :
+#					print(u'%s' % down)
+#					refreshpage(cookie, proxy)
+#					time.sleep(3)
+#					continue
 				if size[-1:]=='K' :
 					act_size=int(len(down)/1024)
-					size=size[:-1]
+					size=int(size[:-1])
+					units = 'K'
 				else :
 					if size[-1:]=='M' :
 						act_size=int(len(down)/1024/1024)
-						size=size[:-1]
+						size=int(size[:-1])
+						units = 'M'
 					else:
 						act_size=len(down)
-				if abs(act_size/size - 1) > 0.1 :
-					print(u'文件大小有误')
+						size=int(size)
+						units = ''
+				if abs(float(act_size)/size -1) * 100 > 10 :       #下载文件与网页上显示的文件大小相差>10%,即下载出错或无法下载
+					print(u'文件大小有误: 源%s%s<>实%s%s' % ( size , units, act_size , units))
+					try:
+						print(u'已下载内容: %s' % down[:100])
+					except :
+						print(u'未知错误')
+					time.sleep(5)
+					refreshpage(cookie, proxy)
 					continue
 				else:
-					with open(filename, 'wb')as code:
-						code.write(down)
 					print(u'下载完成')
+					return_code = bid
 					break
 			else:
 				print(u'下载错误 (%s): %s' %(response.status_code, response.content))
-		except Exception as e:
-			print(u'下载错误 (%s)' % e)
-	return retry_times
+	return return_code
 
 def getSearchList(search_book_name, proxy):
 	dict = []
@@ -246,7 +262,7 @@ def delete_book(bids, cookie, proxy):
 #			print(u'bids : %s' % bids)
 	data = {'uid': uid, 'bids': bids}
 	url = u'http://cn.epubee.com/app_books/deletemybooks.asmx/deletemybooks'
-	my_header = headers
+	my_header = header
 	my_header['Cookie'] = cookie_str
 	my_header['Referer'] = 'http://cn.epubee.com/files.aspx'
 	try:
@@ -261,16 +277,19 @@ def do_login(uuid,uupwd, cookie,proxy):
 	cookie_str = 'ASP.NET_SessionId='+str(cookie.get('ASP.NET_SessionId'))+'; leftshow=1'
 	data = {'eID':uuid,'userpassword':uupwd}
 	url="http://cn.epubee.com/keys/retrieve_cloud_id.asmx/Retrieve"
-	my_header = header
+	my_header = headers
 	my_header['Cookie'] = cookie_str
 	my_header['Referer'] = 'http://cn.epubee.com/files.aspx'
-	try:
+#	try:
+	if 1 :
 		response = requests.post(url, headers=my_header, json=data,proxies=proxy)
 		if response.status_code == 200:
 			update_cookies(response.content.decode())
 			print(u'登陆%s帐号成功' % uuid)
-	except Exception as e:
-		print(u'登陆帐号失败%s' % e)
+		else:
+			print(u'登陆帐号失败：%s' % response.content)
+#	except Exception as e:
+#		print(u'登陆帐号失败%s' % e)
 	return
 
 def main():
@@ -287,8 +306,8 @@ def main():
 #	bs指定搜索结果前N项添加入书库
 	bs = 3
 #	eid,epwd指定用户email及密码(已经设置了Email或Name，不能用ID登录.)
-	email="xxxxx@163.com"
-	epwd=""
+	email="3333333333@163.com"      #设置切换登陆的用户名
+	epwd=""                          #设置切换登陆的密
 
 	path = '/root/down_book/ip.txt'  # 存放爬取ip的文档path，建议为全路径
 	try: 
@@ -337,7 +356,7 @@ def main():
 						break
 		#取书库列表
 		books_list = getBookList(cookie,proxy)
-		del_list = ""
+		done_list = ""
 		if len(books_list) > 0 :
 			print(u'书库数量: %s' % len(books_list) )
 			for bl in range(len(books_list)):
@@ -345,20 +364,23 @@ def main():
 				bid = books_list[bl]['bid']
 				file_size =  books_list[bl]['filesize']
 				print(u'开始下载 : %s, (%s), %s' % (filename, file_size, bid))
-				if download(filename,bid,cookie,loc,proxy, file_size) > 0 :
+				if download(filename,bid,cookie,loc,proxy, file_size) <> '' :
 					print(u'done! %s' % filename)
-					del_list = del_list + bid + ","
+					done_list = done_list + str(bl) + " "
 				else :
 					print(u'下载失败 : %s' % filename)
 		else :
 			print(u'书库没有书')
-		if len(del_list)>0 :
+		if len(done_list)>0 :
 			print(u'通过邮件发送到: %s(Building....)' % cookie.get('uemail'))
-			
-		if len(del_list)>0 :
+			for i in done_list.strip().split(' ') :
+				send_mail(cookie.get('uemail'), books_list[int(i)]['filename'], loc)
+		if len(done_list)>0 :
 			#删除下载成功的书
-			del_list = "0," + del_list[:-1]
-			delete_book(del_list,cookie,proxy)
+			del_list = "0,"
+			for i in done_list.strip().split(' ') :
+				del_list = books_list[int(i)]['bid'] + ','
+			delete_book(del_list[:-1],cookie,proxy)
 	print(u'程序完成')
 	
 if __name__ == '__main__':
